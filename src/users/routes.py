1b035696f3174723289d19033821041cc8e02e03
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-11-25 06:13:15 +0200

    Created routes for users app

---
+++ src/users/routes.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.dependencies import get_db
+from .auth import auth_backend, fastapi_users
+from .models import User
+from .schemas import UserRead, UserCreate, UserUpdate
+
+router = APIRouter()
+
+router.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+router.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@router.get("/users", tags=["users"])
+async def read_users(db: AsyncSession = Depends(get_db)):
+    query = select(User)
+    users = await db.execute(query)
+    users = [user for user in users.scalars()]
+    return users
