0f41a19e141d5769fe2396ae26bed934b4f2c1f2
Mykyta Kuzmytskyi <nykytakuzmytskyi@gmail.com>
2024-11-25 06:17:17 +0200

    Added routes for auth & users API

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from src.users.routes import user_routers
+
 app = FastAPI()
+
+app.include_router(user_routers)
 
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
--- src/users/routes.py
+++ src/users/routes.py
@@ -7,28 +7,28 @@ from .auth import auth_backend, fastapi_users
 from .models import User
 from .schemas import UserRead, UserCreate, UserUpdate
 
-router = APIRouter()
+user_routers = APIRouter()
 
-router.include_router(
+user_routers.include_router(
     fastapi_users.get_auth_router(auth_backend),
     prefix="/auth",
     tags=["auth"],
 )
 
-router.include_router(
+user_routers.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["auth"],
 )
 
-router.include_router(
+user_routers.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
     tags=["users"],
 )
 
 
-@router.get("/users", tags=["users"])
+@user_routers.get("/users", tags=["users"])
 async def read_users(db: AsyncSession = Depends(get_db)):
     query = select(User)
     users = await db.execute(query)
